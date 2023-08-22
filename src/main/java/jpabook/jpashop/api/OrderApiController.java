a81f56cc750ef8111739ce53b79dd03b79da1b46
inho <sayyou0918@gmail.com>
2023-08-22 23:03:13 +0900

    API 성능 컬랙션 조회 - fetch join + hibernate fetch size

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -44,6 +45,16 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderDto> ordersV4(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
         return collect;
     }
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -100,6 +100,10 @@ public class OrderRepository {
         return em.createQuery("select o from Order o" + " join fetch o.member m" + " join fetch o.delivery", Order.class).getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o" + " join fetch o.member m" + " join fetch o.delivery", Order.class).setFirstResult(offset).setMaxResults(limit).getResultList();
+    }
+
     public List<SimpleOrderQueryDto> findOrderDtos() {
         return em.createQuery(
                 "select new jpabook.jpashop.repository.SimpleOrderQueryDto(o.id, o.member.name, o.orderDate, o.status, o.delivery.address) from Order o" + " join o.member m" + " join o.delivery d", SimpleOrderQueryDto.class).getResultList();
